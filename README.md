d4c37d3a40b0a5a956d4c1a799755af71e057407
Marian <neengame@abv.bg>
2025-05-29 15:40:00 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bulgar Development House Robbery
+FiveM House Robbery Script that you can use for free with our [Computer System](https://store.bulgar.dev/package/6271545) and use as a template for your custom heists for the Computer.
+
+# Useful Links
+### [📖 Documentation](https://docs.bulgar.dev/)
+### [🏪 Store](https://store.bulgar.dev/)
+### [💬 Discord](https://discord.gg/HTxN79By6f)
---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'kristiyanpts'
+description 'FiveM House Robbery Script - Computer Version'
+version '1.0.0'
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/*.lua'
+}
+
+client_scripts {
+    'client/**/*.lua',
+}
+
+server_scripts {
+    'server/**/*.lua',
+}
+
+files {
+    "languages/*.json"
+}
+
+escrow_ignore {
+    "client/**/*",
+    "server/**/*",
+    "shared/*.lua",
+}
